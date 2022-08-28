6507df2eba4a1346a8f517c228ac0f17e40762da
i09252003 <108205798+i09252003@users.noreply.github.com>
2022-08-28 22:57:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let yes = 'Hindi kita gusto, pero "Mahal" na kita(type "person" if you want to know who the creator is.)';
+let no = 'putanginamo(type "person" if you want to know who the creator is.)';
+const person = {
+firstName: 'Ibrahim',
+lastName: 'Fernandez',
+feelings: 'unknown',
+age: '18 going 19',
+height: '5.11 ata',
+LovesYou: 'of course!'
+};
+
+console.log('Gusto mo bang malaman kung may gusto ako sayo? "type: yes or no"');
+alert("Hi mahaalll!!!");
+alert('fuck you ╭∩╮（︶︿︶）╭∩╮');
+alert('de jok lang, I LOVE YOU!!! <3')
+alert('This website is just for Michaela Sadiosa Paguibitan only, if you are not that subject then this website is not for you')
